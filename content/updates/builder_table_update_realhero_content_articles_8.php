8ae2b98ec985c8f1d78921ca591c67e1d0de557f
Konrad Fedorczyk <fedek6@gmail.com>
2020-11-17 22:07:09 +0100

    Update

---
+++ content/updates/builder_table_update_realhero_content_articles_8.php
@@ -0,0 +1,23 @@
+<?php namespace RealHero\Content\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateRealheroContentArticles8 extends Migration
+{
+    public function up()
+    {
+        Schema::table('realhero_content_articles', function($table)
+        {
+            $table->string('vertical_cover_orientation')->default('-cover-center');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('realhero_content_articles', function($table)
+        {
+            $table->dropColumn('vertical_cover_orientation');
+        });
+    }
+}
